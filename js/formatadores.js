02df496a1e8ed19e1e18565d4cf95febecce6fba
DiegoBarth <d.alexbarth@gmail.com>
2026-01-30 13:32:10 -0300

    Ajustes nos formatadores

--- js/formatadores.js
+++ js/formatadores.js
@@ -1,19 +1,29 @@
 function formatarMoeda(input) {
-   let valor = input.value.replace(/\D/g, '');
+  let valor = input.value.replace(/\D/g, '');
 
-   if (!valor) {
-      input.value = '';
-      return;
-   }
+  if (!valor) {
+    input.value = '';
+    return;
+  }
 
-   valor = (parseInt(valor, 10) / 100).toFixed(2);
+  const numero = Number(valor) / 100;
 
-   valor = valor
-      .replace('.', ',')
-      .replace(/\B(?=(\d{3})+(?!\d))/g, '.');
-
-   input.value = 'R$ ' + valor;
+  input.value = numero.toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  });
 }
+
+
+function numeroParaMoeda(numero) {
+  if (numero === null || numero === undefined || isNaN(numero)) return '';
+
+  return Number(numero).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  });
+}
+
 
 function converterValor(valor) {
    return Number(
