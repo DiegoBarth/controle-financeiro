e73f99d184138b24ad7f0880b9a51b8f0adf2147
DiegoBarth <d.alexbarth@gmail.com>
2026-01-31 21:06:47 -0300

    Build: gera site em docs para GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
    plugins: [react()],
-   base: '/controle-financeiro/'
+   base: '/controle-financeiro/',
+   build: {
+      outDir: 'docs'
+   }
 })
