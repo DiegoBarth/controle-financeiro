d607b2c949a63f4709cf86ddff8580985a599927
DiegoBarth <d.alexbarth@gmail.com>
2026-01-31 23:00:05 -0300

    feat: criação das rotas e tela de home

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { Receitas } from './pages/Receitas';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './pages/Home';
 import { Gastos } from './pages/Gastos';
 import { Compromissos } from './pages/Compromissos';
+import { Receitas } from './pages/Receitas';
 
 function App() {
-   return (
-      <>
-         <Receitas />
-         <Gastos />
-         <Compromissos />
-      </>
-   );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/gastos" element={<Gastos />} />
+      <Route path="/compromissos" element={<Compromissos />} />
+      <Route path="/receitas" element={<Receitas />} />
+    </Routes>
+  );
 }
 
 export default App;
